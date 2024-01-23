58a5cdebabca7aaa5dfa17582496da9bee200285
TangyMango <129212147+TangyMango@users.noreply.github.com>
2024-01-22 18:40:36 -0600

    Add files via upload

---
+++ Ejercicio_6.js
@@ -0,0 +1,12 @@
+//6.	Crea una función llamada incrementAge que tome como parámetro la edad y 
+//      devuelta la edad incrementada en 5.
+
+var edad = prompt("Edad:");
+
+function incrementAge(edad) {
+    var edad5;
+    edad5 = edad+5;
+    return edad5;
+}
+
+incrementAge(edad);
